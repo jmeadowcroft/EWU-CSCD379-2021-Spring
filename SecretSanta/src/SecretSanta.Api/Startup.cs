65ff4ddf6453eb357f1a8f7f0f2bbd592df461c2
Jenny Meadowcroft <jenny.meadowcroft@intellitect.com>
2021-04-05 09:50:02 -0700

    Added web message

--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -32,7 +32,7 @@ namespace SecretSanta.Api
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello from API!");
+                    await context.Response.WriteAsync("Hello from API! This is a web app!");
                 });
             });
         }
